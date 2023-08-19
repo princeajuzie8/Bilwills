975d5c8abe1a4978914bef722d78fef95eb4b52c
princeajuzie <princeajuzie1@gmail.com>
2023-08-19 22:51:32 +0100

    securing my firebase apikey and other info

--- src/config/firebase/firebase.js
+++ src/config/firebase/firebase.js
@@ -8,16 +8,14 @@ import {getAuth} from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAwVBiUrTD2ql2kIJhzdDILRvKvtqH-wAU",
-  authDomain: "bilwillschat.firebaseapp.com",
-  projectId: "bilwillschat",
-  storageBucket: "bilwillschat.appspot.com",
-  messagingSenderId: "662142604214",
-  appId: "1:662142604214:web:228e3b0a2b0aed2c11e717",
-  measurementId: "G-KWMZS3JE1J"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const Auth = getAuth(app);
 const analytics = getAnalytics(app);
